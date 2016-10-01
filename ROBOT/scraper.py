3705665b1fe166b88adba3bbe2dd9bfc25462845
Robert  Koepp <RJKoepp@Roberts-MacBook-Pro-2.local>
2016-10-01 16:00:38 -0700

    without censorship

--- ROBOT/scraper.py
+++ ROBOT/scraper.py
@@ -8,14 +8,13 @@ def main():
     conn = sqlite3.connect('Top_jokes.db')
     c = conn.cursor()
     submissions = r.get_subreddit('Jokes')
-    post = submissions.get_top(limit = 5)
+    post = submissions.get_top(params={'t': 'all'},limit = None)
     print c.execute("SELECT * FROM Jokes_table")
-    c.execute("INSERT INTO Jokes_table VALUES(1,'stub')")
-    conn.commit()
     for i in post:
         jokeString = i.title.encode('ascii', 'ignore').lower() + "\n" + i.selftext.encode('ascii', 'ignore').lower()
         jokeString = jokeString.decode('utf8')
         print jokeString
         c.execute("INSERT INTO Jokes_table (Joke) VALUES (?)", (jokeString,))
 
+    conn.commit()
 if __name__ == '__main__': main()
