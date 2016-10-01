5f32ebe8ede9a75f74a4c0c262b71353f794c31b
Winson <winson.luk@gmail.com>
2016-10-01 16:23:21 -0700

    update getRandomJoke

--- ROBOT/getRandomJoke.py
+++ ROBOT/getRandomJoke.py
@@ -1,14 +1,12 @@
 import random
 import sqlite3
 
-import praw
-
 def getRandomJoke():
     conn = sqlite3.connect('Top_jokes.db')
     c = conn.cursor()
-    numColumns = c.execute('SELECT COUNT(*) FROM Jokes_table').fetchone()[0]
+    numRows = c.execute('SELECT COUNT(*) FROM Jokes_table').fetchone()[0]
     c.execute('SELECT Joke FROM Jokes_table WHERE id={sentenceId}'.\
-            format(sentenceId=random.randint(1, numColumns))
+            format(sentenceId=random.randint(1, numRows))
             )
     return str(c.fetchone()[0])
 
